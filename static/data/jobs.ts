1710f5cbe9ff1898cb31f45c7ec1250be00345ad
Rafail01 <Rafail01@users.noreply.github.com>
2025-05-23 11:27:50 +0000

    deploy: 63a4b8d59ceed63187989aa4ed5138cf4e389179

---
+++ static/data/jobs.ts
@@ -0,0 +1,12 @@
+export const jobs = [
+    {
+        id: "0",
+        link: "https://www.leukipos.gr/",
+        title: "Λεύκιππος",
+    },
+    {
+        id: "1",
+        link: "https://estia.hua.gr/browse/28586",
+        title: "Η πτυχιακή μου"
+    }
+]
